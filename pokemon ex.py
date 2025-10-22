8ec72590e255546bbf49c3e4cba8b0d9a20c03b3
williamclionzi-cmyk <williamclionzi@gmail.com>
2025-10-22 14:15:09 +0100

    added 1st push

---
+++ pokemon ex.py
@@ -0,0 +1,396 @@
+dex = [
+  {"Bulbasaur":{"number":"1", "type1":"grass", "type2":"poison"}},
+  {"Ivysaur":{"number":"2", "type1":"grass", "type2":"poison"}},
+  {"Venusaur":{"number":"3", "type1":"grass", "type2":"poison"}},
+  {"Charmander":{"number":"4", "type1":"fire", "type2":""}},
+  {"Charmeleon":{"number":"5", "type1":"fire", "type2":""}},
+  {"Charizard":{"number":"6", "type1":"fire", "type2":"flying"}},
+  {"Squirtle":{"number":"7", "type1":"water", "type2":""}},
+  {"Wartortle":{"number":"8", "type1":"water", "type2":""}},
+  {"Blastoise":{"number":"9", "type1":"water", "type2":""}},
+  {"Caterpie":{"number":"10", "type1":"bug", "type2":""}},
+  {"Metapod":{"number":"11", "type1":"bug", "type2":""}},
+  {"Butterfree":{"number":"12", "type1":"bug", "type2":"flying"}},
+  {"Weedle":{"number":"13", "type1":"bug", "type2":"poison"}},
+  {"Kakuna":{"number":"14", "type1":"bug", "type2":"poison"}},
+  {"Beedrill":{"number":"15", "type1":"bug", "type2":"poison"}},
+  {"Pidgeot":{"number":"16", "type1":"normal", "type2":"flying"}},
+  {"Pidgeotto":{"number":"17", "type1":"normal", "type2":"flying"}},
+  {"Pidgeot":{"number":"18", "type1":"normal", "type2":"flying"}},
+  {"Rattata":{"number":"19", "type1":"normal", "type2":""}},
+  {"Raticate":{"number":"20", "type1":"normal", "type2":""}},
+  {"Spearow":{"number":"21", "type1":"normal", "type2":"flying"}},
+  {"Fearow":{"number":"22", "type1":"normal", "type2":"flying"}},
+  {"Ekans":{"number":"23", "type1":"poison", "type2":""}},
+  {"Arbok":{"number":"24", "type1":"poison", "type2":""}},
+  {"Pikachu":{"number":"25", "type1":"electric", "type2":""}},
+  {"Raichu":{"number":"26", "type1":"electric", "type2":""}},
+  {"Ditto":{"number":"27", "type1":"normal", "type2":""}},
+  {"Clefairy":{"number":"28", "type1":"fairy", "type2":""}},
+  {"Clefable":{"number":"29", "type1":"fairy", "type2":""}},
+  {"Jigglypuff":{"number":"30", "type1":"normal", "type2":"fairy"}},
+  {"Wigglytuff":{"number":"31", "type1":"normal", "type2":"fairy"}},
+  {"Zubat":{"number":"32", "type1":"poison", "type2":"flying"}},
+  {"Golbat":{"number":"33", "type1":"poison", "type2":"flying"}},
+  {"Oddish":{"number":"34", "type1":"grass", "type2":"poison"}},
+  {"Gloom":{"number":"35", "type1":"grass", "type2":"poison"}},
+  {"Vileplume":{"number":"36", "type1":"grass", "type2":"poison"}},
+  {"Paras":{"number":"37", "type1":"bug", "type2":"grass"}},
+  {"Parasect":{"number":"38", "type1":"bug", "type2":"grass"}},
+  {"Venonat":{"number":"39", "type1":"bug", "type2":"poison"}},
+  {"Venomoth":{"number":"40", "type1":"bug", "type2":"poison"}},
+  {"Diglett":{"number":"41", "type1":"ground", "type2":""}},
+  {"Dugtrio":{"number":"42", "type1":"ground", "type2":""}},
+  {"Meowth":{"number":"43", "type1":"normal", "type2":""}},
+  {"Persian":{"number":"44", "type1":"normal", "type2":""}},
+  {"Psyduck":{"number":"45", "type1":"water", "type2":""}},
+  {"Golduck":{"number":"46", "type1":"water", "type2":""}},
+  {"Mankey":{"number":"47", "type1":"fighting", "type2":""}},
+  {"Primeape":{"number":"48", "type1":"fighting", "type2":""}},
+  {"Growlithe":{"number":"49", "type1":"fire", "type2":""}},
+  {"Arcanine":{"number":"50", "type1":"fire", "type2":""}},
+  {"Poliwag":{"number":"51", "type1":"water", "type2":""}},
+  {"Poliwhirl":{"number":"52", "type1":"water", "type2":""}},
+  {"Poliwrath":{"number":"53", "type1":"water", "type2":"fighting"}},
+  {"Abra":{"number":"54", "type1":"psychic", "type2":""}},
+  {"Kadabra":{"number":"55", "type1":"psychic", "type2":""}},
+  {"Alakazam":{"number":"56", "type1":"psychic", "type2":""}},
+  {"Machop":{"number":"57", "type1":"fighting", "type2":""}},
+  {"Machoke":{"number":"58", "type1":"fighting", "type2":""}},
+  {"Machamp":{"number":"59", "type1":"fighting", "type2":""}},
+  {"Bellsprout":{"number":"60", "type1":"grass", "type2":"poison"}},
+  {"Weepinbell":{"number":"61", "type1":"grass", "type2":"poison"}},
+  {"Victreebel":{"number":"62", "type1":"grass", "type2":"poison"}},
+  {"Tentacool":{"number":"63", "type1":"water", "type2":"poison"}},
+  {"Tentacruel":{"number":"64", "type1":"water", "type2":"poison"}},
+  {"Slowbro":{"number":"65", "type1":"water", "type2":"psychic"}},
+  {"Slowpoke":{"number":"66", "type1":"water", "type2":"psychic"}},
+  {"Seel":{"number":"67", "type1":"water", "type2":""}},
+  {"Dewgong":{"number":"68", "type1":"water", "type2":"ice"}},
+  {"Shellder":{"number":"69", "type1":"water", "type2":""}},
+  {"Cloyster":{"number":"70", "type1":"water", "type2":"ice"}},
+  {"Gastly":{"number":"71", "type1":"ghost", "type2":"poison"}},
+  {"Haunter":{"number":"72", "type1":"ghost", "type2":"poison"}},
+  {"Gengar":{"number":"73", "type1":"ghost", "type2":"poison"}},
+  {"Onix":{"number":"74", "type1":"rock", "type2":"ground"}},
+  {"Drowzee":{"number":"75", "type1":"psychic", "type2":""}},
+  {"Hypno":{"number":"76", "type1":"psychic", "type2":""}},
+  {"Krabby":{"number":"77", "type1":"water", "type2":""}},
+  {"Kingler":{"number":"78", "type1":"water", "type2":""}},
+  {"Voltorb":{"number":"79", "type1":"electric", "type2":""}},
+  {"Electrode":{"number":"80", "type1":"electric", "type2":""}},
+  {"Exeggcute":{"number":"81", "type1":"grass", "type2":"psychic"}},
+  {"Exeggutor":{"number":"82", "type1":"grass", "type2":"psychic"}},
+  {"Cubone":{"number":"83", "type1":"ground", "type2":""}},
+  {"Marowak":{"number":"84", "type1":"ground", "type2":""}},
+  {"Hitmonlee":{"number":"85", "type1":"fighting", "type2":""}},
+  {"Hitmonchan":{"number":"86", "type1":"fighting", "type2":""}},
+  {"Lickitung":{"number":"87", "type1":"normal", "type2":""}},
+  {"Koffing":{"number":"88", "type1":"poison", "type2":""}},
+  {"Weezing":{"number":"89", "type1":"poison", "type2":""}},
+  {"Rhyhorn":{"number":"90", "type1":"ground", "type2":"rock"}},
+  {"Rhydon":{"number":"91", "type1":"ground", "type2":"rock"}},
+  {"Chansey":{"number":"92", "type1":"normal", "type2":""}},
+  {"Kangaskhan":{"number":"93", "type1":"normal", "type2":""}},
+  {"Horsea":{"number":"94", "type1":"water", "type2":""}},
+  {"Seadra":{"number":"95", "type1":"water", "type2":""}},
+  {"Goldeen":{"number":"96", "type1":"water", "type2":""}},
+  {"Seaking":{"number":"97", "type1":"water", "type2":""}},
+  {"Staryu":{"number":"98", "type1":"water", "type2":""}},
+  {"Starmie":{"number":"99", "type1":"water", "type2":"psychic"}},
+  {"Mr. Mime":{"number":"100", "type1":"psychic", "type2":"fairy"}},
+  {"Scyther":{"number":"101", "type1":"bug", "type2":"flying"}},
+  {"Jynx":{"number":"102", "type1":"ice", "type2":"psychic"}},
+  {"Electabuzz":{"number":"103", "type1":"electric", "type2":""}},
+  {"Magby":{"number":"104", "type1":"fire", "type2":""}},
+  {"Pinsir":{"number":"105", "type1":"bug", "type2":""}},
+  {"Tauros":{"number":"106", "type1":"normal", "type2":""}},
+  {"Magikarp":{"number":"107", "type1":"water", "type2":""}},
+  {"Gyarados":{"number":"108", "type1":"water", "type2":"flying"}},
+  {"Lapras":{"number":"109", "type1":"water", "type2":"ice"}},
+  {"Ditto":{"number":"110", "type1":"normal", "type2":""}},
+  {"Eevee":{"number":"111", "type1":"normal", "type2":""}},
+  {"Vaporeon":{"number":"112", "type1":"water", "type2":""}},
+  {"Jolteon":{"number":"113", "type1":"electric", "type2":""}},
+  {"Flareon":{"number":"114", "type1":"fire", "type2":""}},
+  {"Porygon":{"number":"115", "type1":"normal", "type2":""}},
+  {"Omanyte":{"number":"116", "type1":"rock", "type2":"water"}},
+  {"Omastar":{"number":"117", "type1":"rock", "type2":"water"}},
+  {"Kabuto":{"number":"118", "type1":"rock", "type2":"water"}},
+  {"Kabutops":{"number":"119", "type1":"rock", "type2":"water"}},
+  {"Aerodactyl":{"number":"120", "type1":"rock", "type2":"flying"}},
+  {"Snorlax":{"number":"121", "type1":"normal", "type2":""}},
+  {"Articuno":{"number":"122", "type1":"ice", "type2":"flying"}},
+  {"Zapdos":{"number":"123", "type1":"electric", "type2":"flying"}},
+  {"Moltres":{"number":"124", "type1":"fire", "type2":"flying"}},
+  {"Dratini":{"number":"125", "type1":"dragon", "type2":""}},
+  {"Dragonair":{"number":"126", "type1":"dragon", "type2":""}},
+  {"Dragonite":{"number":"127", "type1":"dragon", "type2":"flying"}},
+  {"Mewtwo":{"number":"128", "type1":"psychic", "type2":""}},
+  {"Mew":{"number":"151", "type1":"psychic", "type2":"water"}},
+  {"Ponyta":{"number":"6", "type1":"fire", "type2":"flying"}},
+  {"Pikachu":{"number":"1", "type1":"electric", "type2":"poison"}}, 
+  {"Blastoise":{"number":"2", "type1":"water", "type2":"flying"}}, 
+  {"Bulbasaur":{"number":"3", "type1":"grass", "type2":"poison"}}, 
+  {"Haunter":{"number":"4", "type1":"ghost", "type2":"poison"}}, 
+  {"Squirtle":{"number":"5", "type1":"water", "type2":""}}
+]
+ 
+print("\n############################")
+print("#### POKEMON BATTLE #####")
+print("##########################\n")
+ 
+print("Available Pokemons: Pikachu, Bulbasaur, Blastoise, Hauntly, Squirtle, Ponyta  ")
+
+#pokemons
+pokemonsDex =[
+{"Pikachu":{"number":"1", "type1":"electric", "type2":"flying"}},
+{"Blastoise":{"number":"2", "type1":"water", "type2":"water"}}, 
+{"Bulbasaur":{"number":"3", "type1":"grass", "type2":"poison"}}, 
+{"Haunter":{"number":"4", "type1":"ghost", "type2":"poison"}}, 
+{"Ponyta":{"number":"6", "type1":"fire", "type2":"flying"}},
+{"Squirtle":{"number":"5", "type1":"water", "type2":"water"}}
+]
+
+# Define type effectiveness
+TYPE_EFFECTIVENESS = {
+    "FIRE": {"FIRE": 0.5, "WATER": 0.5, "GRASS": 2, "ELECTRIC": 1},
+    "WATER": {"FIRE": 2, "WATER": 0.5, "GRASS": 0.5, "ELECTRIC": 0.5},
+    "GRASS": {"FIRE": 0.5, "WATER": 2, "GRASS": 0.5, "ELECTRIC": 1},
+    "ELECTRIC": {"FIRE": 1, "WATER": 2, "GRASS": 0.5, "ELECTRIC": 0.5},
+}
+
+
+# Class to define moves
+class Move:
+    def __init__(self, name, type, power):
+        self.name = name
+        self.type = type
+        self.power = power
+
+    def get_effectiveness_multiplier(self, target_pokemon):
+        multiplier = 1
+        for t in target_pokemon.types:
+            multiplier *= TYPE_EFFECTIVENESS.get(self.type, {}).get(t, 1)
+        return multiplier
+
+# Class to define Pokémon
+class Pokemon:
+    def __init__(self, name, types, max_hp, attack, defense, moves):
+        self.name = name
+        self.types = types
+        self.max_hp = max_hp
+        self.current_hp = max_hp
+        self.attack = attack
+        self.defense = defense
+        self.moves = moves
+        self.is_fainted = False
+
+    def take_damage(self, damage):
+        self.current_hp -= damage
+        if self.current_hp <= 0:
+            self.current_hp = 0
+            self.is_fainted = True
+            print(f"{self.name} fainted!")
+        else:
+            print(f"{self.name} now has {self.current_hp}/{self.max_hp} HP.")
+
+    def is_alive(self):
+        return not self.is_fainted
+
+    def display_status(self):
+        print(f"--- {self.name} ---")
+        print(f"HP: {self.current_hp}/{self.max_hp}")
+        print(f"Type(s): {', '.join(self.types)}")
+
+# Class to manage battles
+class Battle:
+    def __init__(self, trainer1, trainer2):
+        self.trainer1 = trainer1
+        self.trainer2 = trainer2
+
+    def damage_calculation(self, attacking_pokemon, move, defending_pokemon):
+        effectiveness = move.get_effectiveness_multiplier(defending_pokemon)
+        damage = int(
+            0.5 * (attacking_pokemon.attack / defending_pokemon.defense) * move.power * effectiveness
+        )
+        return damage, effectiveness
+
+    def turn(self, attacking_pokemon, move, defending_pokemon):
+        damage, effectiveness = self.damage_calculation(attacking_pokemon, move, defending_pokemon)
+
+        print(f"{attacking_pokemon.name} used {move.name}!")
+        if effectiveness > 1:
+            print("It's super effective!")
+        elif effectiveness < 1:
+            print("It's not very effective...")
+
+        defending_pokemon.take_damage(damage)
+
+    def is_over(self):
+        return not self.trainer1.has_active_pokemon() or not self.trainer2.has_active_pokemon()
+
+    def get_winner(self):
+        if not self.trainer1.has_active_pokemon():
+            return self.trainer2
+        elif not self.trainer2.has_active_pokemon():
+            return self.trainer1
+        return None
+
+# Class to manage trainers and user interaction
+class Trainer:
+    def __init__(self, name, team):
+        self.name = name
+        self.team = team
+        self.active_pokemon_index = 0
+
+    def get_active_pokemon(self):
+        return self.team[self.active_pokemon_index]
+
+    def has_active_pokemon(self):
+        return self.get_active_pokemon().is_alive()
+
+    def choose_action(self):
+        print("\nWhat will you do?")
+        print("1. Fight")
+        print("2. Switch Pokémon")
+        while True:
+            try:
+                choice = int(input("> "))
+                if choice in [1, 2]:
+                    return choice
+                else:
+                    print("Invalid choice. Please enter 1 or 2.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+    def choose_move(self):
+        active_pokemon = self.get_active_pokemon()
+        print("\nChoose a move:")
+        for i, move in enumerate(active_pokemon.moves):
+            print(f"{i + 1}. {move.name}")
+        while True:
+            try:
+                choice = int(input("> "))
+                if 1 <= choice <= len(active_pokemon.moves):
+                    return active_pokemon.moves[choice - 1]
+                else:
+                    print(f"Invalid choice. Please enter a number between 1 and {len(active_pokemon.moves)}.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+    def choose_pokemon_to_switch(self):
+        print("\nSwitch to which Pokémon?")
+        for i, pokemon in enumerate(self.team):
+            if i != self.active_pokemon_index and pokemon.is_alive():
+                print(f"{i + 1}. {pokemon.name} ({pokemon.current_hp}/{pokemon.max_hp})")
+        while True:
+            try:
+                choice = int(input("> "))
+                if 1 <= choice <= len(self.team) and self.team[choice - 1].is_alive():
+                    self.active_pokemon_index = choice - 1
+                    print(f"{self.name} switched to {self.get_active_pokemon().name}!")
+                    break
+                else:
+                    print("Invalid choice. Please select a valid, non-fainted Pokémon.")
+            except ValueError:
+                print("Invalid input. Please enter a number.")
+
+# Function to set up sample data
+def create_sample_data():
+    tackle = Move("Tackle", "NORMAL", 40)
+    ember = Move("Ember", "FIRE", 40)
+    water_gun = Move("Water Gun", "WATER", 40)
+    vine_whip = Move("Vine Whip", "GRASS", 40)
+    thunder_shock = Move("Thunder Shock", "ELECTRIC", 40)
+
+    charmander = Pokemon("Charmander", ["FIRE"], 100, 12, 10, [ember, tackle])
+    squirtle = Pokemon("Squirtle", ["WATER"], 100, 10, 12, [water_gun, tackle])
+    bulbasaur = Pokemon("Bulbasaur", ["GRASS"], 100, 11, 11, [vine_whip, tackle])
+    pikachu = Pokemon("Pikachu", ["ELECTRIC"], 100, 13, 9, [thunder_shock, tackle])
+
+    return [charmander, squirtle, bulbasaur, pikachu]
+
+# Function to get player's team
+def get_player_team(available_pokemon):
+    team = []
+    print("\nSelect your team of 2 Pokémon:")
+    for i, p in enumerate(available_pokemon):
+        print(f"{i + 1}. {p.name}")
+
+    while len(team) < 2:
+        try:
+            choice = int(input(f"Choose Pokémon #{len(team) + 1}: "))
+            if 1 <= choice <= len(available_pokemon):
+                pokemon = available_pokemon[choice - 1]
+                if pokemon not in team:
+                    team.append(pokemon)
+                    print(f"{pokemon.name} added to your team!")
+                else:
+                    print("That Pokémon is already on your team.")
+            else:
+                print(f"Invalid choice. Please enter a number between 1 and {len(available_pokemon)}.")
+        except ValueError:
+                print("Invalid input. Please enter a number.")
+    return team
+
+# Main game loop
+def main():
+    available_pokemon = create_sample_data()
+
+    player_name = input("Enter your name, Trainer: ")
+    player_team = get_player_team(available_pokemon)
+    player_trainer = Trainer(player_name, player_team)
+
+    computer_team = [p for p in available_pokemon if p not in player_team]
+    if not computer_team:
+        computer_team = [any.choice(available_pokemon)]
+    computer_trainer = Trainer("Computer", computer_team)
+
+    print("\n" + "=" * 30)
+    print("        A WILD BATTLE BEGINS!")
+    print("=" * 30)
+
+    battle = Battle(player_trainer, computer_trainer)
+
+    while not battle.is_over():
+        print("\n" + "-" * 30)
+        player_trainer.get_active_pokemon().display_status()
+        computer_trainer.get_active_pokemon().display_status()
+        print("-" * 30)
+
+        player_action = player_trainer.choose_action()
+        if player_action == 1:
+            player_move = player_trainer.choose_move()
+            battle.turn(player_trainer.get_active_pokemon(), player_move, computer_trainer.get_active_pokemon())
+        else:
+            player_trainer.choose_pokemon_to_switch()
+
+        if battle.is_over():
+            break
+
+        print("\nComputer's turn...")
+        computer_move = any.choice(computer_trainer.get_active_pokemon().moves)
+        battle.turn(computer_trainer.get_active_pokemon(), computer_move, player_trainer.get_active_pokemon())
+
+        if not player_trainer.has_active_pokemon():
+            if len([p for p in player_trainer.team if p.is_alive()]) > 0:
+                print(f"{player_trainer.name}, your active Pokémon has fainted!")
+                player_trainer.choose_pokemon_to_switch()
+        if not computer_trainer.has_active_pokemon():
+            if len([p for p in computer_trainer.team if p.is_alive()]) > 0:
+                print(f"{computer_trainer.name}'s active Pokémon has fainted!")
+                for i, p in enumerate(computer_trainer.team):
+                    if p.is_alive():
+                        computer_trainer.active_pokemon_index = i
+                        print(f"Computer switched to {computer_trainer.get_active_pokemon().name}!")
+                        break
+
+    winner = battle.get_winner()
+    if winner:
+        print(f"\nCongratulations, {winner.name} wins the battle!")
+    else:
+        print("\nThe battle is a draw!")
+
+if __name__ == "__main__":
+    main()
